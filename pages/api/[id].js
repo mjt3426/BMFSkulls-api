550c58a1dbe49caeccc124d30a07bf42b25c2973
mjt3426 <35671703+mjt3426@users.noreply.github.com>
2021-08-09 17:40:03 -0400

    Update [id].js

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,193 +1,121 @@
-import Head from 'next/head'
+import {INFURA_ADDRESS, ADDRESS, ABI} from "../../config.js"
 import Web3 from "web3";
-import { useState, useEffect } from 'react';
 
-import {ADDRESS, ABI} from "../config.js"
+// import the json containing all metadata. not recommended, try to fetch the database from a middleware if possible, I use MONGODB for example
+import traits from "../../database/traitsfinal.json";
 
-export default function Mint() {
+const infuraAddress = INFURA_ADDRESS
 
-  // FOR WALLET
-  const [signedIn, setSignedIn] = useState(false)
+const skullApi = async(req, res) => {
 
-  const [walletAddress, setWalletAddress] = useState(null)
-
-  // FOR MINTING
-  const [how_many_skulls, set_how_many_skulls] = useState(1)
-
-  const [skullContract, setskullContract] = useState(null)
-
-  // INFO FROM SMART Contract
-
-  const [totalSupply, setTotalSupply] = useState(0)
-
-  const [saleStarted, setSaleStarted] = useState(false)
-
-  const [skullPrice, setskullPrice] = useState(0)
-
-  useEffect( async() => { 
-
-    signIn()
-
-  }, [])
-
-  async function signIn() {
-    if (typeof window.web3 !== 'undefined') {
-      // Use existing gateway
-      window.web3 = new Web3(window.ethereum);
-     
-    } else {
-      alert("No Ethereum interface injected into browser. Read-only access");
-    }
-
-    window.ethereum.enable()
-      .then(function (accounts) {
-        window.web3.eth.net.getNetworkType()
-        // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
-        .then((network) => {console.log(network);if(network != "ropsten"){alert("You are on " + network+ " network. Change network to mainnet or you won't be able to do anything here")} });  
-        let wallet = accounts[0]
-        setWalletAddress(wallet)
-        setSignedIn(true)
-        callContractData(wallet)
-
-  })
-  .catch(function (error) {
-  // Handle error. Likely the user rejected the login
-  console.error(error)
-  })
-  }
-
-//
-
-  async function signOut() {
-    setSignedIn(false)
-  }
+    // SOME WEB3 STUFF TO CONNECT TO SMART CONTRACT
+  const provider = new Web3.providers.HttpProvider(infuraAddress)
+  const web3infura = new Web3(provider);
+  const skullContract = new web3infura.eth.Contract(ABI, ADDRESS)
   
-  async function callContractData(wallet) {
-    // let balance = await web3.eth.getBalance(wallet);
-    // setWalletBalance(balance)
-    const skullContract = new window.web3.eth.Contract(ABI, ADDRESS)
-    setskullContract(skullContract)
 
-    const salebool = await skullContract.methods.saleIsActive().call() 
-    // console.log("saleisActive" , salebool)
-    setSaleStarted(salebool)
 
-    const totalSupply = await skullContract.methods.totalSupply().call() 
-    setTotalSupply(totalSupply)
-
-    const skullPrice = await skullContract.methods.skullPrice().call() 
-    setskullPrice(skullPrice)
-   
-  }
+  // IF YOU ARE USING INSTA REVEAL MODEL, USE THIS TO GET HOW MANY NFTS ARE MINTED
+//   const totalSupply = await skullContract.methods.totalSupply().call();
+//   console.log(totalSupply)
   
-  async function mintBMFSkull(how_many_skulls) {
-    if (skullContract) {
- 
-      const price = Number(skullPrice)  * how_many_skulls 
 
-      const gasAmount = await skullContract.methods.mintBMFSkull(how_many_skulls).estimateGas({from: walletAddress, value: price})
-      console.log("estimated gas",gasAmount)
 
-      console.log({from: walletAddress, value: price})
+// THE ID YOU ASKED IN THE URL
+  const query = req.query.id;
 
-      skullContract.methods
-            .mintBMFSkull(how_many_skulls)
-            .send({from: walletAddress, value: price, gas: String(gasAmount)})
-            .on('transactionHash', function(hash){
-              console.log("transactionHash", hash)
-            })
-          
+
+  // IF YOU ARE USING INSTA REVEAL MODEL, UNCOMMENT THIS AND COMMENT THE TWO LINES BELOW
+//   if(parseInt(query) < totalSupply) {
+  const totalskulls = 2000;
+  if(parseInt(query) < totalskulls) {
+
+
+    // CALL CUSTOM TOKEN NAME IN THE CONTRACT
+    const tokenNameCall = await skullContract.methods.skullNames(query).call();
+    let tokenName = `#${query}${(tokenNameCall === '') ? "" : ` - ${tokenNameCall}`}`
+
+    // IF YOU ARE NOT USING CUSTOM NAMES, JUST USE THIS
+    // let tokenName= `#${query}`
+
+    
+    
+    const signatures = [2528,2763,3833,5568,5858,6585,6812,7154,8412]
+    const trait = traits[parseInt(query)]
+    // const trait = traits[ Math.floor(Math.random() * 8888) ] // for testing on rinkeby 
+
+    // CHECK OPENSEA METADATA STANDARD DOCUMENTATION https://docs.opensea.io/docs/metadata-standards
+    let metadata = {}
+    // IF THE REQUESTED TOKEN IS A SIGNATURE, RETURN THIS METADATA
+    if ( signatures.includes( parseInt( query ) ) ) {
+    
+      metadata = {
+        "name": tokenName,
+        "description": "BoringBananasCo is a community-centered enterprise focussed on preserving our research about the emerging reports that several banana species have begun exhibiting strange characteristics following the recent worldwide pandemic. Our research team located across the globe has commenced efforts to study and document these unusual phenomena. Concerned about parties trying to suppress our research, the team has opted to store our findings on the blockchain to prevent interference. Although this is a costly endeavour, our mission has never been clearer. The fate of the world's bananas depends on it.",
+        "tokenId" : parseInt(query),
+        "image": `https://gateway.pinata.cloud/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [   
+          {
+            "trait_type": "Signature Series",
+            "value": trait["Signature Series"]
+          }    
+        ]
+      }
+      // console.log(metadata)
     } else {
-        console.log("Wallet not connected")
+    // GENERAL SKULL METADATA
+      metadata = {
+        "name": tokenName,
+        "description": "Do you have the strength to hold? BMF Skulls is a secret society hiding right in front of your eyes. Do you have the patience to wait and see?",
+        "tokenId" : parseInt(query),
+        "image": `https://gateway.pinata.cloud/ipfs/QmbEs3ww4ijPGGvr5kLMCtyhc56x9j1wZVXmQzM3Q74tdH`,
+        "external_url":"https://bmfskulls.art/",
+        "attributes": [          
+            {
+              "trait_type": "Background",
+              "value": "No peeking"
+            },
+            {
+              "trait_type": "Skull",
+              "value": "No peeking"
+            },
+            {
+              "trait_type": "Suits",
+              "value": "No peeking"
+            },
+            {
+              "trait_type": "Eyes",
+              "value": "No peeking"
+            },
+            {
+              "trait_type": "Face",
+              "value": "No peeking"
+            },
+            {
+              "trait_type": "Hats",
+              "value": "No peeking"
+            },
+    
+        ]
+      }
+      
+      // console.log(metadata)
+
     }
     
-  };
+    res.statusCode = 200
+    res.json(metadata)
+  } else {
+    res.statuscode = 404
+    res.json({error: "The skull you requested is out of range"})
+
+  }
+
+
+  // this is after the reveal
 
   
+}
 
-
-
-  return (
-    <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Boring Bananas Company</title>
-        <link rel="icon" href="/images/favicon.jpg" />
-
-        <meta property="og:title" content="Boring Bananas Co." key="ogtitle" />
-        <meta property="og:description" content="Here at Boring Bananas company, we specialise in the world's finest digital bananas. We've put together a team spanning 3 continents, to bring you some of the most ‍NUTRITIOUS and DELICIOUS
-bananas out known to man." key="ogdesc" />
-        <meta property="og:type" content="website" key="ogtype" />
-        <meta property="og:url" content="https://boringbananas.co/" key="ogurl"/>
-        <meta property="og:image" content="https://boringbananas.co/images/Hola.gif" key="ogimage"/>
-        <meta property="og:site_name" content="https://boringbananas.co/" key="ogsitename" />
-
-        <meta name="twitter:card" content="summary_large_image" key="twcard"/>
-        <meta property="twitter:domain" content="boringbananas.co" key="twdomain" />
-        <meta property="twitter:url" content="https://boringbananas.co/" key="twurl" />
-        <meta name="twitter:title" content="Boring Bananas Co." key="twtitle" />
-        <meta name="twitter:description" content="Here at boring Bananas company, we specialise in the world's finest digital bananas. We've put together a team spanning 3 continents, to bring you some of the most ‍NUTRITIOUS and DELICIOUS
-bananas out known to man." key="twdesc" />
-        <meta name="twitter:image" content="https://boringbananas.co/images/Hola.gif" key="twimage" />
-      </Head>
-
-
-      <div >
-          <div className="flex items-center justify-between w-full border-b-2	pb-6">
-            <a href="/" className=""><img src="images/Hola.gif" width="108" alt="" className="logo-image" /></a>
-            <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-              <a href="/#about" className="text-4xl text-white hover:text-black m-6">About</a>
-              <a href="/mint" className="text-4xl text-white hover:text-black m-6">Mint!</a>
-              <a href="/#traits" className="text-4xl text-white hover:text-black m-6">Banana traits</a>
-              <a href="/#roadmap" className="text-4xl text-white hover:text-black m-6">Roadmap</a>
-              <a href="/#team" className="text-4xl text-white hover:text-black m-6">Team</a>
-              <a href="/#contact" className="text-4xl text-white hover:text-black m-6">Contact</a>
-              <a href="https://twitter.com/boringbananasco" className="text-4xl  hover:text-white m-6 text-blau">TWITTER</a>
-              <a href="https://discord.gg/8Wk9Hp6UyV" className="text-4xl  hover:text-white m-6 text-blau">DISCORD</a>
-            </nav>
-             
-          </div>
-          <div className="flex auth my-8 font-bold  justify-center items-center vw2">
-            {!signedIn ? <button onClick={signIn} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Connect Wallet with Metamask</button>
-            :
-            <button onClick={signOut} className="montserrat inline-block border-2 border-black bg-white border-opacity-100 no-underline hover:text-black py-2 px-4 mx-4 shadow-lg hover:bg-blue-500 hover:text-gray-100">Wallet Connected: {walletAddress}</button>}
-          </div>
-        </div>
-
-        <div className="md:w-2/3 w-4/5">
-       
-        
-          <div className="mt-6 border-b-2 py-6">
-
-            <div className="flex flex-col items-center">
-
-                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL SKULLS MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 1000</span></span>
-
-                <div id="mint" className="flex justify-around  mt-8 mx-6">
-                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">GIMME</span>
-                  
-                  <input 
-                                      type="number" 
-                                      min="1"
-                                      max="20"
-                                      value={how_many_skulls}
-                                      onChange={ e => set_how_many_skulls(e.target.value) }
-                                      name="" 
-                                      className="Poppitandfinchsans pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
-                                  />
-                  
-                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">SKULLS!</span>
-    
-                </div>
-                {saleStarted ? 
-                <button onClick={() => mintBMFSkull(how_many_skulls)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_skulls} skulls for {(skullPrice * how_many_skulls) / (10 ** 18)} ETH + GAS</button>        
-                  : <button className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
-            
-              }
-                
-            </div> 
-            </div>
- 
-          </div>  
-    </div>  
-    )
-  }
+export default skullApi
