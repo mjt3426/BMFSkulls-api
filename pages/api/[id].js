b89b956f5f2a3a11b962a3042508e9b652e8ecd0
mjt3426 <35671703+mjt3426@users.noreply.github.com>
2021-08-09 16:50:43 -0400

    Update [id].js

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -6,7 +6,7 @@ import traits from "../../database/traitsfinal.json";
 
 const infuraAddress = INFURA_ADDRESS
 
-const skulApi = async(req, res) => {
+const skullApi = async(req, res) => {
 
     // SOME WEB3 STUFF TO CONNECT TO SMART CONTRACT
   const provider = new Web3.providers.HttpProvider(infuraAddress)
