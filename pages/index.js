ee2586c17d8ea1f6a823d6b3437ecd764a41f853
mjt3426 <35671703+mjt3426@users.noreply.github.com>
2021-08-09 14:51:51 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Mint() {
   const [walletAddress, setWalletAddress] = useState(null)
 
   // FOR MINTING
-  const [how_many_bananas, set_how_many_bananas] = useState(1)
+  const [how_many_skulls, set_how_many_skulls] = useState(1)
 
-  const [bananaContract, setBananaContract] = useState(null)
+  const [skullContract, setskullContract] = useState(null)
 
   // INFO FROM SMART Contract
 
@@ -22,7 +22,7 @@ export default function Mint() {
 
   const [saleStarted, setSaleStarted] = useState(false)
 
-  const [bananaPrice, setBananaPrice] = useState(0)
+  const [skullPrice, setskullPrice] = useState(0)
 
   useEffect( async() => { 
 
@@ -65,33 +65,33 @@ export default function Mint() {
   async function callContractData(wallet) {
     // let balance = await web3.eth.getBalance(wallet);
     // setWalletBalance(balance)
-    const bananaContract = new window.web3.eth.Contract(ABI, ADDRESS)
-    setBananaContract(bananaContract)
+    const skullContract = new window.web3.eth.Contract(ABI, ADDRESS)
+    setskullContract(skullContract)
 
-    const salebool = await bananaContract.methods.saleIsActive().call() 
+    const salebool = await skullContract.methods.saleIsActive().call() 
     // console.log("saleisActive" , salebool)
     setSaleStarted(salebool)
 
     const totalSupply = await bananaContract.methods.totalSupply().call() 
     setTotalSupply(totalSupply)
 
-    const bananaPrice = await bananaContract.methods.bananaPrice().call() 
-    setBananaPrice(bananaPrice)
+    const bananaPrice = await bananaContract.methods.skullPrice().call() 
+    setskullPrice(bananaPrice)
    
   }
   
-  async function mintBanana(how_many_bananas) {
-    if (bananaContract) {
+  async function mintBMFSkull(how_many_skulls) {
+    if (skullContract) {
  
-      const price = Number(bananaPrice)  * how_many_bananas 
+      const price = Number(skullPrice)  * how_many_skull 
 
-      const gasAmount = await bananaContract.methods.mintBoringBanana(how_many_bananas).estimateGas({from: walletAddress, value: price})
+      const gasAmount = await skullContract.methods.mintBMFSkull(how_many_skulls).estimateGas({from: walletAddress, value: price})
       console.log("estimated gas",gasAmount)
 
       console.log({from: walletAddress, value: price})
 
-      bananaContract.methods
-            .mintBoringBanana(how_many_bananas)
+      skullContract.methods
+            .mintBMFSkull(how_many_skulls)
             .send({from: walletAddress, value: price, gas: String(gasAmount)})
             .on('transactionHash', function(hash){
               console.log("transactionHash", hash)
@@ -160,7 +160,7 @@ bananas out known to man." key="twdesc" />
 
             <div className="flex flex-col items-center">
 
-                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL BANANAS MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 8888</span></span>
+                <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL SKULLS MINTED:  <span className="text-blau text-6xl"> {!signedIn ?  <>-</>  :  <>{totalSupply}</> } / 1000</span></span>
 
                 <div id="mint" className="flex justify-around  mt-8 mx-6">
                   <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">GIMME</span>
@@ -169,17 +169,17 @@ bananas out known to man." key="twdesc" />
                                       type="number" 
                                       min="1"
                                       max="20"
-                                      value={how_many_bananas}
-                                      onChange={ e => set_how_many_bananas(e.target.value) }
+                                      value={how_many_skulls}
+                                      onChange={ e => set_how_many_skulls(e.target.value) }
                                       name="" 
                                       className="Poppitandfinchsans pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
                                   />
                   
-                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">BANANAS!</span>
+                  <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold">SKULLS!</span>
     
                 </div>
                 {saleStarted ? 
-                <button onClick={() => mintBanana(how_many_bananas)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_bananas} bananas for {(bananaPrice * how_many_bananas) / (10 ** 18)} ETH + GAS</button>        
+                <button onClick={() => mintBanana(how_many_bananas)} className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">MINT {how_many_skulls} skullss for {(skullPrice * how_many_skulls) / (10 ** 18)} ETH + GAS</button>        
                   : <button className="mt-4 Poppitandfinchsans text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">SALE IS NOT ACTIVE OR NO WALLET IS CONNECTED</button>        
             
               }
