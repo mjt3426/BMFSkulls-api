de7e4385216829db19cbd43804456b1e91bebfae
mjt3426 <35671703+mjt3426@users.noreply.github.com>
2021-08-09 13:21:04 -0400

    Update [id].js

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -6,12 +6,12 @@ import traits from "../../database/traitsfinal.json";
 
 const infuraAddress = INFURA_ADDRESS
 
-const skullApi = async(req, res) => {
+const bananaApi = async(req, res) => {
 
     // SOME WEB3 STUFF TO CONNECT TO SMART CONTRACT
   const provider = new Web3.providers.HttpProvider(infuraAddress)
   const web3infura = new Web3(provider);
-  const skullContract = new web3infura.eth.Contract(ABI, ADDRESS)
+  const bananaContract = new web3infura.eth.Contract(ABI, ADDRESS)
   
 
 
@@ -27,12 +27,12 @@ const skullApi = async(req, res) => {
 
   // IF YOU ARE USING INSTA REVEAL MODEL, UNCOMMENT THIS AND COMMENT THE TWO LINES BELOW
 //   if(parseInt(query) < totalSupply) {
-  const totalskulls = 2000;
-  if(parseInt(query) < totalskulls) {
+  const totalBananas = 8888;
+  if(parseInt(query) < totalBananas) {
 
 
     // CALL CUSTOM TOKEN NAME IN THE CONTRACT
-    const tokenNameCall = await skullsContract.methods.Names(query).call();
+    const tokenNameCall = await bananaContract.methods.bananaNames(query).call();
     let tokenName = `#${query}${(tokenNameCall === '') ? "" : ` - ${tokenNameCall}`}`
 
     // IF YOU ARE NOT USING CUSTOM NAMES, JUST USE THIS
@@ -47,19 +47,20 @@ const skullApi = async(req, res) => {
     // CHECK OPENSEA METADATA STANDARD DOCUMENTATION https://docs.opensea.io/docs/metadata-standards
     let metadata = {}
     // IF THE REQUESTED TOKEN IS A SIGNATURE, RETURN THIS METADATA
-    function validateDate() {
-    pickedDate = Date.parse("016-Aug-2021".replace(/-/g, " "));
-    todaysDate = new Date();
-    todaysDate.setHours(0, 0, 0, 0);
-    dateDifference = Math.abs(Number(todaysDate) - pickedDate);
-    //7 Days=604800000ms
-    if (dateDifference > 86400000) {
-       metadata = {
+    if ( signatures.includes( parseInt( query ) ) ) {
+    
+      metadata = {
         "name": tokenName,
         "description": "BoringBananasCo is a community-centered enterprise focussed on preserving our research about the emerging reports that several banana species have begun exhibiting strange characteristics following the recent worldwide pandemic. Our research team located across the globe has commenced efforts to study and document these unusual phenomena. Concerned about parties trying to suppress our research, the team has opted to store our findings on the blockchain to prevent interference. Although this is a costly endeavour, our mission has never been clearer. The fate of the world's bananas depends on it.",
         "tokenId" : parseInt(query),
-        "image": `https://gateway.pinata.cloud/ipfs/QmbEs3ww4ijPGGvr5kLMCtyhc56x9j1wZVXmQzM3Q74tdH`,
-        "external_url":"https://bmfskulls.art/"
+        "image": `https://gateway.pinata.cloud/ipfs/${trait["imageIPFS"]}`,
+        "external_url":"https://www.boringbananas.co",
+        "attributes": [   
+          {
+            "trait_type": "Signature Series",
+            "value": trait["Signature Series"]
+          }    
+        ]
       }
       // console.log(metadata)
     } else {
@@ -99,10 +100,11 @@ const skullApi = async(req, res) => {
 
     }
     
+    res.statusCode = 200
     res.json(metadata)
   } else {
     res.statuscode = 404
-    res.json({error: "The skull you requested is out of range"})
+    res.json({error: "The banana you requested is out of range"})
 
   }
 
@@ -112,4 +114,4 @@ const skullApi = async(req, res) => {
   
 }
 
-export default skullApi
+export default bananaApi
